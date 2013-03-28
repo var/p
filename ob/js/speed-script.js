746fdd201e07c436381b8f288980634032b0ff6f
Vimal Atreya Ramaka <vimmy2492@gmail.com>
2013-03-28 01:43:07 -0300

    27

--- ob/js/speed-script.js
+++ ob/js/speed-script.js
@@ -5,6 +5,7 @@ $('.hidden').css('opacity',0);
 window.addEventListener('impress:stepenter', function() {
   $('.hidden.active').animate({'opacity': 1});
   $('.clock').slideUp(0).delay(0).fadeIn(1);
+  $('.fall-from-top').slideDown(1000).delay(0).fadeIn(0);
 });
 window.addEventListener('impress:stepleave', function() {
   $('.hidden.past').animate({'opacity': 0});
